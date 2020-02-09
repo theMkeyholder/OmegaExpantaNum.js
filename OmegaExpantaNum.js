e3f648e33e2ca9096897cafd3fd8a426c29fc129
theMkeyholder <51534449+theMkeyholder@users.noreply.github.com>
2020-02-08 22:00:48 -0500

    Create OmegaExpantaNum.js

---
+++ OmegaExpantaNum.js
@@ -0,0 +1,24 @@
+var OEC = {
+debug: 0,
+notation: "letter"
+}
+class OENnumber {
+  constructor(array) {
+  this.array = array
+  }
+  toNumber() {
+  if (this.array.length = 0) {
+  return this.array[1]
+  } else {
+  temp = ""
+    for (i=this.array.length,i--,>-1) {
+      if (typeof this.array[i] = "number") {
+        if ([1,2,3,4,"E","F","G","H"].indexOf(this.array[i]) != -1) {
+        temp2 = [1,2,3,4,"E","F","G","H"]
+        temp2[temp2.indexOf("this.array[i]")]
+        }
+      }
+    }
+  }
+  }
+}
