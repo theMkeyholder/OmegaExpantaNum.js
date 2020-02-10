058a11b1aade626efc45ff98f72e39e97735463b
theMkeyholder <51534449+theMkeyholder@users.noreply.github.com>
2020-02-09 20:24:38 -0500

    Made toNumber kinda better

--- OmegaExpantaNum.js
+++ OmegaExpantaNum.js
@@ -8,14 +8,14 @@ class OENnumber {
   }
   toNumber() {
   if (this.array.length = 0) {
-  return this.array[1]
+  return this.array[0]
   } else {
-  temp = ""
+  temp = this.array[this.array.length-1]
     for (i=this.array.length,i--,>-1) {
       if (typeof this.array[i] = "number") {
         if ([1,2,3,4,"E","F","G","H"].indexOf(this.array[i]) != -1) {
         temp2 = [1,2,3,4,"E","F","G","H"]
-        temp2[temp2.indexOf("this.array[i]")]
+        temp = temp2[temp2.indexOf(this.array[i])+4]+ temp
         }
       }
     }
